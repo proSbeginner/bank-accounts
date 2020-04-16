e27078ca32b775eff306042563e85f07474bea56
Kittipong Puankunta <prosbeginner@hotmail.com>
2020-04-16 21:50:17 +0700

    remove an account by id

--- src/main/java/com/pros/App.java
+++ src/main/java/com/pros/App.java
@@ -7,50 +7,70 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         AccountDao dao = new AccountDao();
 
 
         Scanner sc = new Scanner(System.in);
-        while(true) {
+        while (true) {
             System.out.println("===== Menu =====");
             System.out.println("1. Create an account");
-            System.out.println("2. Get account balance");
+            System.out.println("2. Delete an account");
             System.out.println("3. Show all accounts");
             System.out.print("Please choose: ");
             int choice = sc.nextInt();
             switch (choice) {
-                case 1:
+                case 1: {
                     sc.nextLine();
                     System.out.print("Enter account number: ");
-                    String aNumber = sc.nextLine();
+                    String accountNumber = sc.nextLine();
                     System.out.print("Enter account name: ");
-                    String aName = sc.nextLine();
+                    String accountName = sc.nextLine();
                     System.out.print("Enter account phone: ");
-                    String aPhone = sc.nextLine();
+                    String accountPhone = sc.nextLine();
 
                     Account account = new Account();
-                    account.setNumber(aNumber);
-                    account.setName(aName);
-                    account.setPhone(aPhone);
+                    account.setNumber(accountNumber);
+                    account.setName(accountName);
+                    account.setPhone(accountPhone);
 
                     int recordCount = dao.create(account);
-                    if(recordCount > 0) {
+                    if (recordCount > 0) {
                         System.out.println("An account is created");
                     }
-
                     break;
-                case 2:
-                    System.out.println("XXXXXXX");
-                    break;
-                case 3:
+                }
+                case 2: {
                     List<Account> list = dao.findAll();
-                    for(int i = 0; i < list.size(); i++) {
+                    for (int i = 0; i < list.size(); i++) {
+                        Long accountId = list.get(i).getId();
                         String accountNumber = list.get(i).getNumber();
                         String accountName = list.get(i).getName();
                         String accountPhone = list.get(i).getPhone();
 
-                        String line = String.format("%-15s%-10s%-10s", accountNumber, accountName, accountPhone);
+                        String line = String.format("%-5s%-15s%-10s%-10s", accountId, accountNumber, accountName, accountPhone);
+                        System.out.println(line);
+                    }
+                    System.out.print("Enter account id: ");
+                    Long accountId = sc.nextLong();
+                    Account account = new Account();
+                    account.setId(accountId);
+
+                    int recordCount = dao.delete(account);
+                    if (recordCount > 0) {
+                        System.out.println("An account is deleted");
+                    }
+                    break;
+                }
+                case 3:
+                    List<Account> list = dao.findAll();
+                    for (int i = 0; i < list.size(); i++) {
+                        Long accountId = list.get(i).getId();
+                        String accountNumber = list.get(i).getNumber();
+                        String accountName = list.get(i).getName();
+                        String accountPhone = list.get(i).getPhone();
+
+                        String line = String.format("%-5s%-15s%-10s%-10s", accountId, accountNumber, accountName, accountPhone);
                         System.out.println(line);
                     }
                     break;
--- src/main/java/com/pros/dao/AccountDao.java
+++ src/main/java/com/pros/dao/AccountDao.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class AccountDao {
 
     private Connection getConnection() {
-        String url = "jdbc:sqlserver://localhost:1433;user=SA;password=Passw0rd;databaseName=accountdb";
+        String url = "jdbc:sqlserver://localhost:1434;user=SA;password=Passw0rd;databaseName=accountdb";
         try {
             return DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -28,11 +28,13 @@ public class AccountDao {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM ACCOUNT");
 
             while (resultSet.next()) {
+                Long accountId = resultSet.getLong("id");
                 String accountNumber = resultSet.getString("number");
                 String accountName = resultSet.getString("name");
                 String accountPhone = resultSet.getString("phone");
 
                 Account account = new Account();
+                account.setId(accountId);
                 account.setNumber(accountNumber);
                 account.setName(accountName);
                 account.setPhone(accountPhone);
@@ -47,13 +49,24 @@ public class AccountDao {
     }
 
     public int create(Account account) {
-        // INSERT INTO ACCOUNT(number, name, phone) VALUES(?, ?, ?)
         Connection connection = getConnection();
         try {
             PreparedStatement statement = connection.prepareStatement("INSERT INTO ACCOUNT(number, name, phone) VALUES(?, ?, ?)");
             statement.setString(3, account.getPhone());
             statement.setString(1, account.getNumber());
             statement.setString(2, account.getName());
+            int recordCount = statement.executeUpdate();
+            return recordCount;
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public int delete(Account account) {
+        Connection connection = getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM ACCOUNT WHERE id=?");
+            statement.setString(1, account.getId().toString());
             int recordCount = statement.executeUpdate();
             return recordCount;
         } catch (SQLException e) {
